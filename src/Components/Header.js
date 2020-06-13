811329368b29b3c59683acca597eed73bcc9bf4a
shuja990 <shujaali1234@gmail.com>
2020-06-13 20:57:28 +0500

    adding redux

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,6 @@ class Header extends Component{
 	render(){
 		return (
 			<div>
-			<div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="shuja-ali-075408ab"><a class="LI-simple-link" href='https://pk.linkedin.com/in/shuja-ali-075408ab?trk=profile-badge'>Shuja Ali</a></div>
 				<h1 className="f1">RoboFriends</h1>
 				<CounterButton color={'red'}/>
 			</div>
